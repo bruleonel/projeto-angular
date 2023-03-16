d01f1635af41076a96d1f51b915c975aa082f8d0
Bruna <psbru2012@gmail.com>
2023-03-16 16:31:25 -0300

    componentes criados

--- semana2/exercicio-aula2-gestao-de-funcionarios/src/app/features/features.module.ts
+++ semana2/exercicio-aula2-gestao-de-funcionarios/src/app/features/features.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListagemColaboradoresComponent } from './listagem-colaboradores/listagem-colaboradores.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListagemColaboradoresComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ semana2/exercicio-aula2-gestao-de-funcionarios/src/app/features/listagem-colaboradores/listagem-colaboradores.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem-colaboradores',
+  templateUrl: './listagem-colaboradores.component.html',
+  styleUrls: ['./listagem-colaboradores.component.scss']
+})
+export class ListagemColaboradoresComponent {
+
+}
