43a00aaed7899166a3537116cf62a93792a17544
Bruna <psbru2012@gmail.com>
2023-03-27 22:27:08 -0300

    feat:Adicionando novas funcionalidades

---
+++ semana4/mjv-cosmeticos/src/app/features/sell/models/products.model.ts
@@ -0,0 +1,7 @@
+export interface Produto {
+  descricao: String;
+  preco: number;
+  quantidade: number;
+  imagem: String;
+  disponivel: boolean;
+}
--- semana4/mjv-cosmeticos/src/app/shared/shared.module.ts
+++ semana4/mjv-cosmeticos/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BlueDirective } from './directives/blue.directive';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BlueDirective
   ],
   imports: [
     CommonModule
